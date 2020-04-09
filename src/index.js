68f1b6ffaf52f8749a43749fac778f39b69856b9
Rajat Vijay <rajatvijay5@gmail.com>
2020-04-09 07:52:33 +0530

    user input form setup

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use Parcel to bundle this sandbox, you can find more info about Parcel
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+const createDataSizeInput = (container) => {
+  const input = document.createElement("input");
+  input.type = "text";
+  input.addEventListener("input", (e) => {});
+  input.remo;
+};
+
+const makeSearchInputConditionallyRequired = () => {
+  const algoSelector = document.querySelector("#algoSelection");
+  const searchValueInput = document.querySelector("#search-number");
+  algoSelector.addEventListener("change", (e) => {
+    console.log("init", e.target.value, e.target.values);
+    if (["LINEAR_SEARCH", "BINARY_SEARCH"].includes(e.target.value)) {
+      searchValueInput.setAttribute("required", true);
+    } else {
+      searchValueInput.removeAttribute("required");
+    }
+  });
+};
+
+function init() {
+  makeSearchInputConditionallyRequired();
+}
+
+init();
